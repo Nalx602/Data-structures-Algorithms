f248287bfe0efec3724572008edff5d6ec7123a9
Alex <81243338+Nalx602@users.noreply.github.com>
2022-02-03 23:41:09 +0000

    Implement a hash table

---
+++ DS Hash Tables/lesson84_DS_Hash_Tables.js
@@ -0,0 +1,13 @@
+// Hash tables VS Arrays
+
+//            Arrays      Hash Tables
+//  search    O(n)          O(1)
+//  lookup    O(1)          O(1)
+//  push      O(1)          na
+//  insert    O(n)          O(1)
+//  delete    O(n)          O(1)
+
+// When we want hash tables?
+// When you need quick access to a value;
+// When you don't want to waste time with searching a value;
+// Usually in DBs
